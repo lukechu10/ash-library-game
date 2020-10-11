456dcacbc951ea975cad0c13a39810bd65281597
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-10-11 15:58:30 -0700

    Create 2 lists

--- src/components/GameWrapper.svelte
+++ src/components/GameWrapper.svelte
@@ -12,21 +12,36 @@
     function handleSort(e) {
         booksList = e.detail.items;
     }
+
+    let booksListSecond: { id: number; name: string }[] = [];
+
+    for (let i = 0; i < 10; i++) {
+        booksListSecond.push({ id: i, name: `book second ${i}` });
+    }
+
+    function handleSortSecond(e) {
+        booksListSecond = e.detail.items;
+    }
 </script>
 
 <style>
     section {
-		width: 50%;
+		width: 100%;
 		padding: 0.3em;
 		border: 1px solid black;
         /* this will allow the dragged element to scroll the list */
-		overflow: scroll;
-		height: 600px;
+		overflow: hidden;
+		height: 150px;
 	}
 
-    .book {
+    div {
+        display: inline-block; /* horizontal list */
         padding: 10px;
-        margin: 2px 0;
+        margin: 0 2px;
+    }
+
+    .book {
+        height: 100px;
         border: 2px solid black;
         border-radius: 2px;
     }
@@ -41,3 +56,14 @@
         <div class="book" animate:flip={{ duration: flipDurationMs }}>{book.name}</div>
     {/each}
 </section>
+
+<section
+    use:dndzone={{ items: booksListSecond, flipDurationMs }}
+    on:consider={handleSortSecond}
+    on:finalize={handleSortSecond}
+>
+    {#each booksListSecond as book (book.id)}
+        <div class="book-second" animate:flip={{ duration: flipDurationMs }}>{book.name}</div>
+    {/each}
+</section>
+
