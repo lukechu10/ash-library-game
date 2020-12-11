e5a4173cd85df3227b5deb5bef85a44c91555704
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-12-10 22:58:20 -0800

    Add test for starting game

--- cypress/integration/sorting_spec.js
+++ cypress/integration/sorting_spec.js
@@ -4,6 +4,14 @@ describe("Sorting", () => {
     it("successfully loads", () => {
         cy.visit("http://localhost:3000");
 
-        cy.get("a").click();
+        cy.get('a[href="./game"]').click();
+    });
+
+    it("can start game", () => {
+        cy.visit("http://localhost:3000/game");
+
+        cy.get(".s-btn").click();
+
+        cy.get(".book").should("have.lengthOf", 4);
     });
 });
