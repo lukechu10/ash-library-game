581a1f704ea274bcb3e0955f51b6f48bd5476133
Luke Chu <lukewchu@outlook.com>
2021-05-29 21:33:47 +0000

    Classes page

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -71,7 +71,7 @@
                                         role="menuitem">Mon compte</a
                                     >
                                     <a
-                                        href="/my_classes"
+                                        href="/account/classes"
                                         class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
                                         role="menuitem">Mes classes</a
                                     >
